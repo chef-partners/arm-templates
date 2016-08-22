f8f0b15b62e7fcda0fb1e047d06e4fcfc63667d7
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-08-22 17:25:15 +0100

    Added script for installing the automate server from a debian package

---
+++ scripts/install-automate.sh
@@ -0,0 +1,13 @@
+
+# Script to download, install and configure Chef Automate
+
+# Command line parameters that are required in the script
+# $1 - Automate License file
+# $2 - Key for Chef Automate User
+# $3 - Chef Server URL
+# $4 - FQDN of the Automate Server
+
+wget https://packages.chef.io/stable/ubuntu/14.04/delivery_0.5.125-1_amd64.deb
+dpkg -i delivery_0.5.125-1_amd64.deb
+
+delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4
