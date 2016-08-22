69bef4292c090f9675b0a4c99ae6b1bf18a05158
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-08-22 18:25:16 +0100

    Script required some more parameters for it to run the setup of Automate correctly Redirected the output of the setup command to a file so that the generated passwords can be found

--- scripts/install-automate.sh
+++ scripts/install-automate.sh
@@ -6,8 +6,9 @@
 # $2 - Key for Chef Automate User
 # $3 - Chef Server URL
 # $4 - FQDN of the Automate Server
+# $5 - Delivery Organisation Name
 
 wget https://packages.chef.io/stable/ubuntu/14.04/delivery_0.5.125-1_amd64.deb
 dpkg -i delivery_0.5.125-1_amd64.deb
 
-delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4
+delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4 -e "$5" --configure --no-build-node > /root/automate-setup.log
