9f401c1b62b2bea1471a83590064b916e3ac436a
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-08-22 18:30:10 +0100

    Added a log for the command that was called

--- scripts/install-automate.sh
+++ scripts/install-automate.sh
@@ -8,7 +8,12 @@
 # $4 - FQDN of the Automate Server
 # $5 - Delivery Organisation Name
 
+LOG_FILE="/root/automate-setup.log"
+
+echo "$0 $1 $2 $3 $4 $5" > $LOG_FILE
+echo >> $LOG_FILE
+
 wget https://packages.chef.io/stable/ubuntu/14.04/delivery_0.5.125-1_amd64.deb
 dpkg -i delivery_0.5.125-1_amd64.deb
 
-delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4 -e "$5" --configure --no-build-node > /root/automate-setup.log
+delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4 -e "$5" --configure --no-build-node >> $LOG_FILE
