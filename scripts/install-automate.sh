bdb9fd2a27c6ddefbc3aa52c9246772e4c7e73e5
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-09-07 15:28:43 +0100

    Modified templates to allow the Chef Server to be monitored by OMS Modified the scripts that do this

--- scripts/install-automate.sh
+++ scripts/install-automate.sh
@@ -14,19 +14,38 @@ echo "$0 $1 $2 $3 $4 $5" > $LOG_FILE
 echo >> $LOG_FILE
 
 echo "Downloading Automate" >> $LOG_FILE
-wget https://packages.chef.io/stable/ubuntu/14.04/delivery_0.5.125-1_amd64.deb
+
+# Ensure that the package is not downloaded if it already exists
+if [ ! -f delivery_0.5.125-1_amd64.deb ]
+then
+  wget https://packages.chef.io/stable/ubuntu/14.04/delivery_0.5.125-1_amd64.deb
+fi
 
 echo "Installing Automate" >> $LOG_FILE
-dpkg -i delivery_0.5.125-1_amd64.deb
+
+# Only install if the `delivery-ctl` command does not exist
+DELIVERY=`which delivery-ctl`
+if [ "X$DELIVERY" == "X" ]
+then
+  dpkg -i delivery_0.5.125-1_amd64.deb
+fi
 
 which delivery-ctl >> $LOG_FILE
 
-# Build up the command to be run
-cmd="sudo /usr/bin/delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4 -e "$5" --configure --no-build-node >> $LOG_FILE 2>> $LOG_FILE"
+# Set a home variable for the script
+export HOME="/root"
 
-echo "Setup command" >> $LOG_FILE
+# Build up the command to be run
+cmd="/usr/bin/delivery-ctl setup --license $1 --key $2 --server-url $3 --fqdn $4 -e "$5" --configure --no-build-node"
+
+# Write out the command to a file so that it can be called using the `at` command
+# echo $cmd > "/root/automate-setup.sh"
+
+# Scedule the command to run in 1 minute
+# at -f /root/automate-setup.sh now + 1min 
+
+# echo "Setup command" >> $LOG_FILE
 echo $cmd >> $LOG_FILE
 
 # Execute the command
-`$cmd`
-
+$cmd >> $LOG_FILE 2>> $LOG_FILE
